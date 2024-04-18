0999c22ae7667e6bfe08ffcd00d412ae4d1a97e0
Olakunle64 <olakunleisiaq50@gmail.com>
2024-04-18 23:06:35 +0100

    correct the annotations in task 101

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,14 +1,15 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """This module has a function named <safely_get_value>
     that takes in a dictionary, key, and a default value and return
     the value of the key or None
 """
 from typing import Mapping, Optional, Union, TypeVar, Any
+T = TypeVar("T")
 
 
 def safely_get_value(
-        dct: Mapping, key: Any, default: Union[TypeVar("T"), None] = None
-        ) -> Union[Any, TypeVar("T")]:
+        dct: Mapping, key: Any, default: Union[T, None] = None
+        ) -> Union[Any, T]:
     """This function takes in a dict, key, default value and check if
         a key is present in a dictionary and then return the
         value of the key or None if it doesn't
