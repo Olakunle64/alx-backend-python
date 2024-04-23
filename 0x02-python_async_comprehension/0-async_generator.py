e70aaeede5444160778605361ef50a2388ffb9ba
Olakunle64 <olakunleisiaq50@gmail.com>
2024-04-23 15:30:32 +0100

    adjust

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -15,6 +15,6 @@ async def async_generator() -> AsyncIterator[float]:
         Yields:
             float: A random floating-point number between 1 and 10.
     """
-    for i in range(10):
+    for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 11)
+        yield random.uniform(0, 10)
