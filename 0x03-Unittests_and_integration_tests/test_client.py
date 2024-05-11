217c4d7d63e834fba08c930b0a05f2e236ebc079
Olakunle64 <olakunleisiaq50@gmail.com>
2024-05-11 14:49:35 +0100

    test the public_repo of GithubOrgClient class

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -33,3 +33,27 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_p_repos_url.return_value = payload
         client = GithubOrgClient(org)
         self.assertEqual(client._public_repos_url(), payload)
+
+    @parameterized.expand([
+        (
+            "google",
+            {"repos_url": "https://api.github.com/orgs/google/repos"},
+            [{"name": "repo1"}, {"name": "repo2"}]
+        ),
+        (
+            "abc",
+            {"repos_url": "https://api.github.com/orgs/abc/repos"},
+            [{"name": "repo1"}, {"name": "repo2"}]
+        )
+    ])
+    @patch("client.get_json")
+    def test_public_repos(self, org, payload, repos_list, mock_get_json):
+        "Test the GithubOrgClient.public_repos"
+        with patch.object(GithubOrgClient, "_public_repos_url") as mock_p:
+            mock_p.return_value = payload
+            mock_get_json.return_value = repos_list
+            client = GithubOrgClient(org)
+            self.assertEqual(
+                client.public_repos(), [repo["name"] for repo in repos_list]
+                )
+            mock_get_json.assert_called_once()
