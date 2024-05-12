91442835253cb683a3bd838947543f3ffca31d65
Olakunle64 <olakunleisiaq50@gmail.com>
2024-05-12 19:18:19 +0100

    correct task 6

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -62,7 +62,7 @@ class TestGithubOrgClient(unittest.TestCase):
             self.assertEqual(
                 client.public_repos(), [repo["name"] for repo in payload]
                 )
-            mock_get_json.assert_called_once()
+        mock_get_json.assert_called_once()
 
     @parameterized.expand([
         ({"license": {"key": "my_license"}}, "my_license", True),
